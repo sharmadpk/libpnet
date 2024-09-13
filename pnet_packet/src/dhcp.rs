eb79e691ca2fe01180208eaab3009ff8b0ca33ff
Deepak Sharma <sharmadpk@gmail.com>
2024-09-13 14:36:35 +1200

    retry option analysis after removing magic cookie

--- pnet_packet/src/dhcp.rs
+++ pnet_packet/src/dhcp.rs
@@ -6,7 +6,7 @@ use core::net::Ipv4Addr;
 
 use pnet_base::MacAddr;
 use pnet_macros::packet;
-use pnet_macros_support::types::*;
+use pnet_macros_support::{packet::Packet, types::*};
 
 /// Represents an Dhcp operation.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -98,8 +98,10 @@ pub struct Dhcp {
     pub file: Vec<u8>,
     #[length = "4"]
     pub magic: Vec<u8>,
+    #[length_fn ="dhcp_options_length"]
+    pub options: Vec<DhcpOption>,
     #[payload]
-    pub options: Vec<u8>,
+    pub payload: Vec<u8>,
 }
 
 #[packet]
@@ -112,6 +114,36 @@ pub struct DhcpOption {
     pub value: Vec<u8>,
     #[payload]
     pub payload: Vec<u8>,
+}
+
+
+// Special DHCP option codes
+const DHCP_OPTION_PAD: u8 = 0;
+const DHCP_OPTION_END: u8 = 255;
+const DHCP_OPTION_MAGIC:u8 = 99;
+
+// Location of the DHCP options in the packet
+const DHCP_OPTIONS_OFFSET: usize = 240;
+
+fn dhcp_options_length(packet: &DhcpPacket) -> usize {
+    let mut length = 0;
+    let offset = DHCP_OPTIONS_OFFSET;
+    loop{
+        let code = packet.packet()[offset+length];
+        length += match code {
+            DHCP_OPTION_PAD => 1,
+            DHCP_OPTION_END => 1,
+            DHCP_OPTION_MAGIC => 6,
+            _ => {
+                let len = packet.packet()[offset + length + 1] as usize;
+                2 + len
+            }
+        };
+        if code == DHCP_OPTION_END {
+            break;
+        }
+    }
+    length
 }
 
 #[cfg(test)]
